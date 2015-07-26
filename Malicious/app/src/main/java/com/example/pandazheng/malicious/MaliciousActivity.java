2e6b6ddf2fd2b6388ca77a0dfb9d276ed66e4e7c
pandazheng <543828315@qq.com>
2015-07-26 11:14:36 +0800

    应用组件攻防(Activity)源码
    应用组件攻防(Activity)源码

---
+++ Malicious/app/src/main/java/com/example/pandazheng/malicious/MaliciousActivity.java
@@ -0,0 +1,44 @@
+package com.example.pandazheng.malicious;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MaliciousActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_malicious);
+
+        Intent i = new Intent();
+        i.setComponent(new ComponentName("com.example.pandazheng.testapp","com.example.pandazheng.testapp.Welcome"));
+        startActivity(i);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_malicious, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ TestApp/app/src/main/java/com/example/pandazheng/testapp/Welcome.java
@@ -0,0 +1,15 @@
+package com.example.pandazheng.testapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by pandazheng on 15/7/26.
+ */
+public class Welcome extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+    }
+}
