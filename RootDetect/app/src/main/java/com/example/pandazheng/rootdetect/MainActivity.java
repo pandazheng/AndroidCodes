742af56ac9f6551a9f2984fd05c37c96d072ff17
pandazheng <543828315@qq.com>
2015-07-26 15:40:35 +0800

    检测Android是否Root源码
    检测Android是否Root源码

---
+++ RootDetect/app/src/main/java/com/example/pandazheng/rootdetect/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.pandazheng.rootdetect;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.File;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private Button btn;
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button)findViewById(R.id.btnRoot);
+        tv = (TextView)findViewById(R.id.tvOut);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File file = new File("/system/app/Superuser.apk");
+                Boolean returnvalue = file.exists();
+
+                if (returnvalue == true) {
+                    tv.setText("Device Rooted");
+                }
+                else {
+                    tv.setText("Device is Not Rooted");
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
