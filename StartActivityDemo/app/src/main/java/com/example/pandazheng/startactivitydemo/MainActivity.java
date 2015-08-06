05a5922061fb66798574e6688fd1e6ecf33893aa
pandazheng <543828315@qq.com>
2015-08-06 17:06:09 +0800

    StartActivity Source Code
    StartActivity Source Code

---
+++ StartActivityDemo/app/src/main/java/com/example/pandazheng/startactivitydemo/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.pandazheng.startactivitydemo;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //需要root的手机，哈哈
+        Intent intent = new Intent();
+        ComponentName componentName = new ComponentName("com.tencent.mobileqq","com.tencent.mobileqq.activity.ChatActivity");
+        intent.setComponent(componentName);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
